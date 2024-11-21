b4e4194b51e5fff90025450ef088c7162150ce55
10XYXYX10 <lmcr10100722@gmail.com>
2024-11-21 09:45:33 +0900

    Find the cause of the amplify error. v6.

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { security } from './lib/functions/seculity';
+
+export const middleware = async(request: NextRequest) => {
+  const responseNext = NextResponse.next()
+
+  const jwtEncoded = request.cookies.get('accessToken')?.value;
+  const {result,data} = await security(jwtEncoded);
+
+  const pathName = request.nextUrl.pathname;
+  const userId = Number(pathName.split('/')[2]);//「/user/<認証済みuserId>」
+  
+  if(!result || userId!=data?.id){    
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = '/auth';
+    const response = NextResponse.redirect(redirectUrl)
+    if(response.cookies.has('accessToken')){
+      response.cookies.delete('accessToken')//middlewareを経由してredirectする場合、このアプローチでないとcookieの削除に失敗する。
+    }
+    return response;
+  }
+
+  return responseNext;
+};
+
+export const config = {
+  matcher: ['/user/:path*'],
+};
