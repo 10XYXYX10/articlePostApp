63e4095423bd0a4b8ec3773351c743da0921dc35
10XYXYX10 <lmcr10100722@gmail.com>
2024-11-21 09:52:34 +0900

    Find the cause of the amplify error. v7.

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,20 @@ export const middleware = async(request: NextRequest) => {
 
   const jwtEncoded = request.cookies.get('accessToken')?.value;
   const {result,data} = await security(jwtEncoded);
+  console.log(JSON.stringify({result,data}))
 
   const pathName = request.nextUrl.pathname;
   const userId = Number(pathName.split('/')[2]);//「/user/<認証済みuserId>」
   
-  if(!result || userId!=data?.id){    
-    const redirectUrl = request.nextUrl.clone();
-    redirectUrl.pathname = '/auth';
-    const response = NextResponse.redirect(redirectUrl)
-    if(response.cookies.has('accessToken')){
-      response.cookies.delete('accessToken')//middlewareを経由してredirectする場合、このアプローチでないとcookieの削除に失敗する。
-    }
-    return response;
-  }
+  // if(!result || userId!=data?.id){    
+  //   const redirectUrl = request.nextUrl.clone();
+  //   redirectUrl.pathname = '/auth';
+  //   const response = NextResponse.redirect(redirectUrl)
+  //   if(request.cookies.has('accessToken')){
+  //     response.cookies.delete('accessToken')//middlewareを経由してredirectする場合、このアプローチでないとcookieの削除に失敗する。
+  //   }
+  //   return response;
+  // }
 
   return responseNext;
 };
